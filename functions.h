2696bdbfa05e4e7b81d890258f566482165653c1
hala <halaeissa@aucegypt.edu>
2021-02-08 21:10:41 +0200

    functions added

---
+++ functions.h
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+bool checkSolution();
+bool solveNQueen(int bord[N][N], int col);
+void printBoard(int board[N][N]);
+bool isValid(int board[N][N], int row, int col);
